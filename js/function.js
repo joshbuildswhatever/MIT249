c89ab0ee553bc94f8cb819fb88ba408552e294e9
joshbuildswhatever <84601632+joshbuildswhatever@users.noreply.github.com>
2021-07-15 17:48:18 -0700

    Delete function.js

--- js/function.js
+++
@@ -1,5 +0,0 @@
-/* Display the last time this document was modified */
-var msg = '<p><b>Last Modified: </b>' + document.lastModified + '</p>';
-var el = document.getElementById('footer');
-
-el.innerHTML = msg;
